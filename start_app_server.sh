5d2e2a4d2c6636b520818883c1b73af86873c9f7
Azmat Parveen <azmat@propertyguru.com.sg>
2018-10-03 17:12:48 +0800

    removed secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://azmat:infrared1@ds161262.mlab.com:61262/mytest
+# export MONGODB_CREDENTIALS=mongodb://username1:username1@ds161262.mlab.com:61262/mytest
 
 # finally, run the app in dev mode...
 npm run dev
